cc0f1f3f2c91d772b92e00f850de98f70563c6c9
B.E. Henriksen <e@jiku.ca>
2015-08-08 02:58:42 +0200

    Makes sure logo isn't clickable after exiting intro.

--- client/intro.js
+++ client/intro.js
@@ -6,6 +6,7 @@ Template.intro.events({
   'click': function (event) {
     if (Session.equals("state", "intro")) {
       Session.set("state", "normal");
+      $(event.currentTarget).addClass('disable-clicks');
       SoundEvent.emit('play', {
         data: "Play «Priss»."
       });
