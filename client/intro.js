8cb9e9fdc34037938910460320e020e29307ea89
B.E. Henriksen <e@jiku.ca>
2015-08-08 02:37:37 +0200

    Avoids fading out logo.
    Before changes to vertical sectioning, where the logo should persist on
    top.

--- client/intro.js
+++ client/intro.js
@@ -1,7 +1,6 @@
 Template.intro.events({
   'click': function (event) {
     if (Session.equals("state", "intro")) {
-      $(event.currentTarget).addClass('fadeout');
       Session.set("state", "playing");
 
       SoundEvent.emit('play', {
