2f9448d53192bd87a25ee809e81fb234a1de44ce
B.E. Henriksen <e@jiku.ca>
2015-08-22 23:02:42 +0200

    Cleanup.

--- client/particles_sprites.js
+++ client/particles_sprites.js
@@ -102,27 +102,16 @@ function onDocumentMouseMove( event ) {
 function onDocumentTouchStart( event ) {
 
   if ( event.touches.length === 1 ) {
-
-    // event.preventDefault();
-
     mouseX = event.touches[ 0 ].pageX - windowHalfX;
     mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
   }
-
 }
 
 function onDocumentTouchMove( event ) {
-
   if ( event.touches.length === 1 ) {
-
-    // event.preventDefault();
-
     mouseX = event.touches[ 0 ].pageX - windowHalfX;
     mouseY = event.touches[ 0 ].pageY - windowHalfY;
-
   }
-
 }
 
 function animate() {
@@ -130,7 +119,6 @@ function animate() {
   requestAnimationFrame( animate );
 
   render();
-
 }
 
 function render() {
