65225e5c302dba2fc20ffd13aef9b02744a7adea
B.E. Henriksen <e@jiku.ca>
2016-02-20 23:24:01 +0100

    Lazy-loads core module.

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'mantra-core';
 import initContext from './configs/context';
 
 // modules
-import coreModule from './modules/core';
+import coreModule from '/imports/modules/core';
 
 // init context
 const context = initContext();
