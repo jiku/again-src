361f4619d80cdf0da9b10ece6b61bf11ec95a1e8
B.E. Henriksen <e@jiku.ca>
2016-02-20 22:39:06 +0100

    Bumps to Meteor 1.3b11, etc.
    Also deactivates jiku:visualizers, removes unused Mantra module.

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import initContext from './configs/context';
 
 // modules
 import coreModule from './modules/core';
-import commentsModule from './modules/comments';
 
 // init context
 const context = initContext();
