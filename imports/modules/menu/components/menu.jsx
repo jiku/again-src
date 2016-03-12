a47500d39648afea0cfe897cedda1c66d4bce245
B.E. Henriksen <e@jiku.ca>
2016-03-12 14:58:53 +0100

    Style changes.
    WIP. Not sure what I'm after... Wanted to use .styl, etc.

--- imports/modules/menu/components/menu.jsx
+++ imports/modules/menu/components/menu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {slide as SlidingMenu} from 'react-burger-menu';
 import Radium from 'radium';
 import color from 'color';
+// import Styles from 'react-burger-menu.styl';
 
 // import MenuItem from './MenuItem.jsx';
 // import * as BTMenu from 'meteor/qazuor:animated-border-menu';
@@ -30,18 +31,21 @@ import color from 'color';
 // let SlidingMenu = BurgerMenu.slide
 
 let Example = React.createClass({
-  showSettings: function(event) {
+  toggleMenu: function(event) {
     event.preventDefault();
+    console.log("yay");
+    this.state.isOpen;
   },
   render: function() {
     return (
-      <SlidingMenu styles={styles} placement='bottom'>
-        <a key="0" onClick={ this.showSettings } href="/again/header"><i className="fa fa-fw fa-arrow-up"></i></a>
-        <a key="1" id="home" href="/again/about">About</a>
-        <a key="2" id="about" href="/again/player">Player</a>
-        <a key="3" id="about" href="/again/contribute">Contribute</a>
-        <a key="4" id="contact" href="/again/contact">Contact</a>
-        <a key="5" onClick={ this.showSettings } href="/again/header"><i className="fa fa-fw fa-arrow-down"></i></a>
+      // <SlidingMenu styles={styles} placement='bottom'>
+      <SlidingMenu placement='bottom'>
+        <a key="0" onClick={ this.toggleMenu } id="header" href="/again/header"><i className="fa fa-fw fa-arrow-up"></i></a>
+        <a key="1" onClick={ this.toggleMenu } id="home" href="/again/about">About</a>
+        <a key="2" onClick={ this.toggleMenu } id="about" href="/again/player">Player</a>
+        <a key="3" onClick={ this.toggleMenu } id="about" href="/again/contribute">Contribute</a>
+        <a key="4" onClick={ this.toggleMenu } id="contact" href="/again/contact">Contact</a>
+        <a key="5" onClick={ this.toggleMenu } id="footer" href="/again/footer"><i className="fa fa-fw fa-arrow-down"></i></a>
       </SlidingMenu>
     );
   }
@@ -55,49 +59,49 @@ const Menu = ({}) => (
   </div>
 );
 
-let styles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '50%',
-    bottom: '36px'
-  },
-  bmBurgerBars: {
-    background: '#97f'
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px'
-  },
-  bmCross: {
-    background: '#bdc3c7'
-  },
-  bmMenu: {
-    background: '#000',
-    fontSize: '1.15em',
-    display: 'flex',
-    'align-items': 'center'
-  },
-  bmMorphShape: {
-    fill: '#373a47'
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    display: 'flex',
-    'font-family': 'jikutypeVector',
-    'text-decoration': 'none',
-    'text-transform': 'lowercase',
-    'font-size': '4px',
-    'flex-flow': 'row',
-    'flex-direction': 'row',
-    'justify-content': 'center',
-    'align-items': 'center'
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0.3)'
-  },
-};
+// let styles = {
+//   bmBurgerButton: {
+//     position: 'fixed',
+//     width: '36px',
+//     height: '30px',
+//     left: '50%',
+//     bottom: '36px'
+//   },
+//   bmBurgerBars: {
+//     background: '#97f'
+//   },
+//   bmCrossButton: {
+//     height: '24px',
+//     width: '24px'
+//   },
+//   bmCross: {
+//     background: '#bdc3c7'
+//   },
+//   bmMenu: {
+//     background: '#000',
+//     fontSize: '1.15em',
+//     display: 'flex',
+//     'align-items': 'center'
+//   },
+//   bmMorphShape: {
+//     fill: '#373a47'
+//   },
+//   bmItemList: {
+//     color: '#b8b7ad',
+//     display: 'flex',
+//     'font-family': 'jikutypeVector',
+//     'text-decoration': 'none',
+//     'text-transform': 'lowercase',
+//     'font-size': '4px',
+//     'flex-flow': 'row',
+//     'flex-direction': 'row',
+//     'justify-content': 'center',
+//     'align-items': 'center'
+//   },
+//   bmOverlay: {
+//     background: 'rgba(0, 0, 0, 0.3)'
+//   },
+// };
 
 // let styles2 = {
 // };
