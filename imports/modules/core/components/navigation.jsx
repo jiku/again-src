8274b0950b6078a5c378a795686b70d0d97caa87
B.E. Henriksen <e@jiku.ca>
2016-03-14 22:51:58 +0100

    Adds some text.

--- imports/modules/core/components/navigation.jsx
+++ imports/modules/core/components/navigation.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Navigation = () => (
   <div>
-    Sign up for<br/>
-    - Notifications<br/>
-    - Newsletter (rare)<br/>
+    <br/><br/>
+    To avoid technical issues, the app/site is being partially remade in <a href="http://mantrajs.com/">Mantra</a>.<br/>
+    <br/>
+    Sign up for interim notifications and a newsletter <a href="notify">here</a>.<br/>
+    You can follow progress <a href="">here</a>, but it's not really indicative.
+    More soon!<br/>
   </div>
 );
 
